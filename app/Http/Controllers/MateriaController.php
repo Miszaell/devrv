238217230d4774037cfd4913df69b99f1db43dcc
Miszaell <misaelvgm011@gmail.com>
2023-03-12 23:01:29 -0600

    fix

--- app/Http/Controllers/MateriaController.php
+++ app/Http/Controllers/MateriaController.php
@@ -2,47 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\materia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\materia;
 
 class MateriaController extends Controller
 {
-    public function list(){
-        $materias=materia::all();
+    function list() {
+        $materias = materia::all();
         return $materias;
     }
 
-    public function find(Request $req){
-        $materia=materia::find($req->id);
+    public function find(Request $req)
+    {
+        $materia = materia::find($req->id);
         return $materia;
     }
-    public function save(Request $req){
-        if($req->id == 0){
+    public function save(Request $req)
+    {
+        if ($req->id == 0) {
             $materia = new materia();
-        }
-        else{
-            $materia =materia::find($req->id);
+        } else {
+            $materia = materia::find($req->id);
         }
 
-        if(intval($req->id_brand) != 0){
+        if (intval($req->id_brand) != 0) {
             $materia->id_brand = intval($req->id_brand);
         }
 
         $materia->nombre = $req->nombre;
         $materia->horas_semana = $req->horas_semana;
 
-
         $materia->save();
 
         return $materia;
     }
 
-    public function delete($id){
-        $materia = materia::find(intval($id));
+    public function delete($id)
+    {
+        $materia = DB::table('materias')->where('id', intval($id))->delete();
+        $resp = array([
+            "id" => 0,
+            "nombre" => "",
+            "horas_semana" => "",
+        ]);
+        $status = $materia == 1 ? 200 : 500;
+        return response()->json($resp, $status);
 
-        $materia->delete();
-
-        return "OK";
     }
 }
--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -2,52 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\profesor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\profesor;
 
 class ProfesorController extends Controller
 {
-    public function list(){
-        $profesors=DB::table('profesors')
-        ->leftjoin('materias', 'profesors.id_materia', '=','materias.id')
-        ->select('profesors.*','materias.nombre as nombre_materia')
-        ->orderBy('profesors.updated_at','desc')
-        ->get();
+    function list() {
+        $profesors = DB::table('profesors')
+            ->leftjoin('materias', 'profesors.id_materia', '=', 'materias.id')
+            ->select('profesors.*', 'materias.nombre as nombre_materia')
+            ->orderBy('profesors.updated_at', 'desc')
+            ->get();
         return $profesors;
     }
 
-    public function find(Request $req){
-        $profesor=profesor::find($req->id);
+    public function find(Request $req)
+    {
+        $profesor = profesor::find($req->id);
         return $profesor;
     }
-    public function save(Request $req){
-        if($req->id == 0){
+    public function save(Request $req)
+    {
+        if ($req->id == 0) {
             $profesor = new profesor();
-        }
-        else{
-            $profesor =profesor::find($req->id);
+        } else {
+            $profesor = profesor::find($req->id);
         }
 
-        if(intval($req->id_brand) != 0){
-            $profesor->id_brand = intval($req->id_brand);
+        if (intval($req->id_materia) != 0) {
+            $profesor->id_materia = intval($req->id_materia);
         }
 
         $profesor->nombre = $req->nombre;
         $profesor->grado_estudio = $req->grado_estudio;
         $profesor->id_materia = $req->id_materia;
 
-
         $profesor->save();
 
         return $profesor;
     }
 
-    public function delete($id){
-        $profesor = profesor::find($id);
+    public function delete($id)
+    {
+        $profesor = DB::table('profesors')->where('id', intval($id))->delete();
+        $resp = array([
+            "id" => 0,
+            "nombre" => "",
+            "grado_estudio" => "",
+            "id_materia" => 0,
+        ]);
+        $status = $profesor == 1 ? 200 : 500;
+        return response()->json($resp, $status);
 
-        $profesor->delete();
-
-        return "OK";
     }
 }
